ed4824e8010de54d79850cabe89361254406dcb3
progspectar <59685434+progspectar@users.noreply.github.com>
2023-09-18 12:44:14 +0700

    test

--- src/test/java/ru/job4j/assertj/SimpleConvertTest.java
+++ src/test/java/ru/job4j/assertj/SimpleConvertTest.java
@@ -3,17 +3,13 @@ package ru.job4j.assertj;
 import org.assertj.core.data.Index;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SimpleConvertTest {
-    @Test
-    void checkArray() {
-        SimpleConvert simpleConvert = new SimpleConvert();
-        String[] array = simpleConvert.toArray("first", "second", "three", "four", "five");
-        assertThat(array).hasSize(5)
-                .contains("second")
-                .contains("first", Index.atIndex(0))
-                .containsAnyOf("zero", "second", "six")
-                .doesNotContain("first", Index.atIndex(1));
-    }
+
 }
