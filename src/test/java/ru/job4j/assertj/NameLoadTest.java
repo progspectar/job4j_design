62da0186d5d8ba1eb0b506b9428ed9eb1425ca92
progspectar <59685434+progspectar@users.noreply.github.com>
2023-09-18 09:43:43 +0700

    4. Утверждения с исключениями [#504886]

--- src/test/java/ru/job4j/assertj/NameLoadTest.java
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -1,9 +1,47 @@
 package ru.job4j.assertj;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NameLoadTest {
+    /*example of formatted string: "=key1=value1"-> Map<String, String>, limit 2*/
+    @Test
+    void checkNamesLength() {
+        NameLoad nameLoad = new NameLoad();
+        String[] names = {};
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("is empty");
+    }
+
+    @Test
+    void checkFormatParsedString1() {
+        NameLoad nameLoad = new NameLoad();
+        String[] names = {"key1!value1"};
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain the symbol '='");
+    }
+
+    @Test
+    void checkFormatParsedString2() {
+        NameLoad nameLoad = new NameLoad();
+        String[] names = {"=value1"};
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain a key");
+    }
+
+    @Test
+    void checkFormatParsedString3() {
+        NameLoad nameLoad = new NameLoad();
+        String[] names = {"key1="};
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain a value");
+    }
+
     @Test
     void checkEmpty() {
         NameLoad nameLoad = new NameLoad();
