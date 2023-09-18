b96f6d4cf583f871b449a9b451ee7e5e02b77f66
progspectar <59685434+progspectar@users.noreply.github.com>
2023-09-18 09:42:20 +0700

    test

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+}
