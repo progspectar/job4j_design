69353965af90578cce9928fc531682081f4007b8
progspectar <59685434+progspectar@users.noreply.github.com>
2023-09-18 10:16:10 +0700

    test

---
+++ src/test/java/ru/job4j/assertj/SimpleConvertTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.assertj;
+
+import org.assertj.core.data.Index;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SimpleConvertTest {
+    @Test
+    void checkArray() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        String[] array = simpleConvert.toArray("first", "second", "three", "four", "five");
+        assertThat(array).hasSize(5)
+                .contains("second")
+                .contains("first", Index.atIndex(0))
+                .containsAnyOf("zero", "second", "six")
+                .doesNotContain("first", Index.atIndex(1));
+    }
+}
