b7924010f234ef40f4ac2b6d8dc876dd89fdf7fe
progspectar <59685434+progspectar@users.noreply.github.com>
2023-09-19 20:20:05 +0700

    test

---
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -0,0 +1,11 @@
+package ru.job4j.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Balancer {
+    public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
+        /* тут должна быть реализация */
+    }
+}
