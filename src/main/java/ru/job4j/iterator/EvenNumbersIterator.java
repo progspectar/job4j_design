4b70ffefae1c5b181d82468daa18fcb456a4958d
progspectar <59685434+progspectar@users.noreply.github.com>
2023-09-19 14:09:25 +0700

    3. Итератор четных чисел [#150]

---
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -0,0 +1,24 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+
+public class EvenNumbersIterator implements Iterator<Integer> {
+
+    private int[] data;
+    private int index;
+
+    public EvenNumbersIterator(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+
+    }
+
+    @Override
+    public Integer next() {
+
+    }
+
+}
