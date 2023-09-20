c62beba5356479f78fe6ef9f7f96d69a64776d13
progspectar <59685434+progspectar@users.noreply.github.com>
2023-09-20 14:29:14 +0700

    исправление #1 7. Балансир [#505083]

--- src/main/java/ru/job4j/iterator/Balancer.java
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -8,8 +8,8 @@ public class Balancer {
     public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
         int i = 0;
         while (source.hasNext()) {
-            nodes.get(i).add(source.next());
-            i = (++i == nodes.size()) ? 0 : i;
+            nodes.get(i++).add(source.next());
+            i = i == nodes.size() ? 0 : i;
         }
     }
 }
