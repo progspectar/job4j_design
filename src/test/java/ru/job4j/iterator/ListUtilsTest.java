d896d56c22bc0980750e833c04fc93e93c7b92b3
progspectar <59685434+progspectar@users.noreply.github.com>
2023-09-28 16:38:24 +0700

    7. ListIterator [#350217]

--- src/test/java/ru/job4j/iterator/ListUtilsTest.java
+++ src/test/java/ru/job4j/iterator/ListUtilsTest.java
@@ -1,10 +1,14 @@
 package ru.job4j.iterator;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ListUtilsTest {
 
@@ -31,5 +35,28 @@ class ListUtilsTest {
     void whenAddAfter() {
         ListUtils.addAfter(input, 0, 2);
         assertThat(input).hasSize(3).containsSequence(1, 2, 3);
+    }
+
+
+    @Test
+    public void whenRemoveIf() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        ListUtils.removeIf(list, i -> i % 2 == 0);
+        assertThat(list).hasSize(3).containsSequence(1, 3, 5);
+    }
+
+    @Test
+    public void whenReplaceIf() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        ListUtils.replaceIf(list, i -> i % 2 == 0, 9);
+        assertThat(list).hasSize(5).containsSequence(1, 9, 3, 9, 5);
+    }
+
+    @Test
+    public void whenRemoveAll() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 4, 5, 6));
+        List<Integer> elements = new ArrayList<>(Arrays.asList(1, 6));
+        ListUtils.removeAll(list, elements);
+        assertThat(list).hasSize(3).containsSequence(2, 4, 5);
     }
 }
