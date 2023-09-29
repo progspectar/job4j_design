94185f30c7344c7ecf5c59522d36ff2a53a0dcd3
progspectar <59685434+progspectar@users.noreply.github.com>
2023-09-29 20:09:36 +0700

    исправление замечаний 7. ListIterator

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -9,14 +9,12 @@ public class ListUtils {
 
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator(index);
-        iterator.add(value);
+        list.listIterator(index).add(value);
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
-        Objects.checkIndex(index++, list.size());
-        ListIterator<T> iterator = list.listIterator(index);
-        iterator.add(value);
+        Objects.checkIndex(index, list.size());
+        list.listIterator(index + 1).add(value);
     }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
@@ -38,6 +36,6 @@ public class ListUtils {
     }
 
     public static <T> void removeAll(List<T> list, List<T> elements) {
-        removeIf(list, l -> (elements.contains(l)));
+        removeIf(list, elements::contains);
     }
 }
