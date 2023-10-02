ed0f41088b0809f6eaace6567bbc359a12f16fb1
progspectar <59685434+progspectar@users.noreply.github.com>
2023-10-02 16:39:46 +0700

    test

---
+++ src/main/java/ru/job4j/question/Analyze.java
@@ -0,0 +1,11 @@
+package ru.job4j.question;
+
+import java.util.Set;
+
+public class Analize {
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+
+    }
+
+}
---
+++ src/main/java/ru/job4j/question/Info.java
@@ -0,0 +1,17 @@
+package ru.job4j.question;
+
+public class Info {
+
+    private int added;
+    private int changed;
+    private int deleted;
+
+    public Info(int added, int changed, int deleted) {
+        this.added = added;
+        this.changed = changed;
+        this.deleted = deleted;
+    }
+
+    /* Геттеры, сеттеры, equals() & hashCode() */
+
+}
