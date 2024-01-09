d3b288bf9bf6fe08bc5099674706c48dd9621b1f
progspectar <progspectar@gmail.com>
2024-01-09 14:02:41 +0700

    реализация 3. Статистика по коллекции.

--- src/main/java/ru/job4j/question/Info.java
+++ src/main/java/ru/job4j/question/Info.java
@@ -40,8 +40,12 @@ public class Info {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Info info = (Info) o;
         return added == info.added && changed == info.changed && deleted == info.deleted;
     }
