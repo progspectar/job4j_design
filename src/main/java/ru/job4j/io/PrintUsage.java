7db64d1616c2292a0b1f04ce4afc033027f6699d
progspectar <progspectar@gmail.com>
2024-02-14 01:28:23 +0700

    2.1. FileInputStream [#4898 #479669]

---
+++ src/main/java/ru/job4j/io/PrintUsage.java
@@ -0,0 +1,27 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+public class PrintUsage {
+
+    public static void main(String[] args) {
+        try (PrintStream stream = new PrintStream(new FileOutputStream("data/print.txt"))) {
+            stream.println("Из PrintStream в FileOutputStream");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (PrintStream stream = new PrintStream(new FileOutputStream("data/print.txt"));
+             PrintWriter writer = new PrintWriter("data/write.txt")) {
+            stream.println("Из PrintStream в FileOutputStream");
+            stream.write("Новая строка".getBytes());
+            writer.println("Новое сообщение");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
