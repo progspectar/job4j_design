8173126e3580a07e3d7e5b2bc70fd3560d6e32ac
progspectar <progspectar@gmail.com>
2024-02-14 01:45:29 +0700

    2.1. FileInputStream [#4898 #479669]

--- src/main/java/ru/job4j/io/ReadFile.java
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -1,22 +1,38 @@
 package ru.job4j.io;
 
-import java.io.FileInputStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReadFile {
     public static void main(String[] args) {
-        try (FileInputStream input = new FileInputStream("data/input.txt")) {
-            StringBuilder text = new StringBuilder();
-            int read;
-            while ((read = input.read()) != -1) {
-                text.append((char) read);
-            }
-            System.out.println(text);
-            String[] lines = text.toString().split(System.lineSeparator());
-            for (String line : lines) {
-                System.out.println(line);
-            }
+        try (BufferedReader input = new BufferedReader(new FileReader("data/input.txt"))) {
+            input.lines().forEach(System.out::println);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+
+//package ru.job4j.io;
+//
+//import java.io.FileInputStream;
+//
+//public class ReadFile {
+//    public static void main(String[] args) {
+//        try (FileInputStream input = new FileInputStream("data/input.txt")) {
+//            StringBuilder text = new StringBuilder();
+//            int read;
+//            while ((read = input.read()) != -1) {
+//                text.append((char) read);
+//            }
+//            System.out.println(text);
+//            String[] lines = text.toString().split(System.lineSeparator());
+//            for (String line : lines) {
+//                System.out.println(line);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
