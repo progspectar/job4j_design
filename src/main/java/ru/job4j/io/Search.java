3ac66a7dd677dedf8a965f752d9dc02a04fdf0ec
progspectar <progspectar@gmail.com>
2024-02-18 06:01:57 +0700

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,31 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validateArgs(args);
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validateArgs(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Please provide two arguments: <starting_folder> <file_extension>");
+        }
+        Path startDir = Path.of(args[0]);
+        String fileExtension = args[1];
+        if (!Files.exists(startDir)) {
+            throw new IllegalArgumentException("The starting directory does not exist: " + args[0]);
+        }
+        if (!Files.isDirectory(startDir)) {
+            throw new IllegalArgumentException("The starting directory is not a directory: " + args[0]);
+        }
+        if (!fileExtension.matches("\\.\\w+")) {
+            throw new IllegalArgumentException(String.format("Non valid file extension: \"%s\". Provide a valid file extension like (e.g.: '.txt'.)", fileExtension));
+        }
     }
 }
