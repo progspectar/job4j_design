0fc0c66fb1bf232bfd00b409f892f5de1594b8e4
progspectar <progspectar@gmail.com>
2024-02-18 03:41:01 +0700

    10. Сканирование файловой системы [#106929]

--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -12,39 +12,5 @@ import java.util.function.Predicate;
 import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class SearchFiles implements FileVisitor<Path> {
-    private final List<Path> Paths = new ArrayList<>();
-    private final Predicate<Path> condition;
 
-    public SearchFiles(Predicate<Path> condition) {
-        this.condition = condition;
-    }
-
-
-    @Override
-    public FileVisitResult preVisitDirectory(Path directory, BasicFileAttributes attributes) throws IOException {
-        return CONTINUE;
-    }
-
-    @Override
-    public FileVisitResult visitFile(Path file, BasicFileAttributes attributes) throws IOException {
-        if (condition.test(file)) {
-            Paths.add(file);
-
-        }
-        return CONTINUE;
-    }
-
-    @Override
-    public FileVisitResult visitFileFailed(Path file, IOException exception) throws IOException {
-        return CONTINUE;
-    }
-
-    @Override
-    public FileVisitResult postVisitDirectory(Path directory, IOException exception) throws IOException {
-        return CONTINUE;
-    }
-
-    public List<Path> getPaths() {
-        return Paths;
-    }
 }
