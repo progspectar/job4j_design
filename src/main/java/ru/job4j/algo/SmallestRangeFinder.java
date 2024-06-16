60a2ddb2ce853154b851412a90b99ab47f71f1de
progspectar <progspectar@gmail.com>
2024-06-16 23:11:52 +0700

    Алгоритмы с Hash структурами [#505154]

--- src/main/java/ru/job4j/algo/SmallestRangeFinder.java
+++ src/main/java/ru/job4j/algo/SmallestRangeFinder.java
@@ -4,24 +4,23 @@ import java.util.Arrays;
 
 public class SmallestRangeFinder {
     public static int[] findSmallestRange(int[] nums, int k) {
-        int left = 0;
-        int rigth = 1;
-        boolean flag = false;
-        for (; rigth < nums.length; rigth++) {
-            if (nums[rigth - 1] == nums[rigth]) {
-                left = rigth;
+        int count = 1;
+        for (int right = 1; right < nums.length; right++) {
+            if (nums[right] == nums[right - 1]) {
+                count = 1;
+            } else {
+                count++;
             }
-            if ((rigth - left) + 1 == k) {
-                flag = true;
-                break;
+            if (count == k) {
+                return new int[]{right - k + 1, right};
             }
         }
-        return flag ? new int[]{left, rigth} : null;
+        return null;
     }
 
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5};
-        int k = 4;
+        int k = 2;
         int[] result = findSmallestRange(nums, k);
         if (result != null) {
             System.out.println("Наименьший диапазон с " + k + " различными элементами: " + Arrays.toString(result));
