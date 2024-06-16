29c883da15e043586e497f517a37934b090e65bf
progspectar <progspectar@gmail.com>
2024-06-16 23:13:02 +0700

    Алгоритмы с Hash структурами [#505154]

---
+++ src/main/java/ru/job4j/algo/LongestUniqueSubstring.java
@@ -0,0 +1,12 @@
+package ru.job4j.algo;
+
+public class LongestUniqueSubstring {
+    public static String longestUniqueSubstring(String str) {
+      return str;
+    }
+
+    public static void main(String[] args) {
+        var str = "abcdefgh";
+        System.out.println(longestUniqueSubstring(str));
+    }
+}
