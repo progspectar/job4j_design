b8f4f2495373548a6e2b1d206d8fe7907bfa78b0
progspectar <progspectar@gmail.com>
2024-06-16 23:13:29 +0700

    Алгоритмы с Hash структурами [#505154]

--- src/main/java/ru/job4j/algo/LongestUniqueSubstring.java
+++ src/main/java/ru/job4j/algo/LongestUniqueSubstring.java
@@ -1,8 +1,29 @@
 package ru.job4j.algo;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestUniqueSubstring {
     public static String longestUniqueSubstring(String str) {
-      return str;
+        var res = new StringBuilder();
+        var sb = new StringBuilder();
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (set.contains(c)) {
+                sb.setLength(0);
+                sb.append(c);
+                set.clear();
+                set.add(c);
+            } else {
+                set.add(c);
+                sb.append(c);
+                if (sb.length() > res.length()) {
+                    res = new StringBuilder(sb);
+                }
+            }
+        }
+        return res.toString();
     }
 
     public static void main(String[] args) {
