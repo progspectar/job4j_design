be122e79fb81a9033f8900774fad6ccdd3a35769
progspectar <progspectar@gmail.com>
2024-06-17 23:19:10 +0700

    Очереди и стеки [#505157]

--- src/main/java/ru/job4j/algo/Brackets.java
+++ src/main/java/ru/job4j/algo/Brackets.java
@@ -1,12 +1,26 @@
 package ru.job4j.algo;
 
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 class Brackets {
     public boolean isValid(String s) {
-        return false;
-    }
-
-    public static void main(String[] args) {
-        Brackets brackets = new Brackets();
-        System.out.println(brackets.isValid(""));
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('{', '}');
+        map.put('[', ']');
+        Deque<Character> stack = new LinkedList<>();
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else if (map.get(c) != null) {
+                stack.push(map.get(c));
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
     }
 }
