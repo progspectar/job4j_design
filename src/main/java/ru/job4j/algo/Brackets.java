2a3dfd2a749ae3931ba4fa04bd1e2caaea3872b0
progspectar <progspectar@gmail.com>
2024-06-17 20:37:51 +0700

    Очереди и стеки [#505157]

---
+++ src/main/java/ru/job4j/algo/Brackets.java
@@ -0,0 +1,12 @@
+package ru.job4j.algo;
+
+class Brackets {
+    public boolean isValid(String s) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Brackets brackets = new Brackets();
+        System.out.println(brackets.isValid(""));
+    }
+}
