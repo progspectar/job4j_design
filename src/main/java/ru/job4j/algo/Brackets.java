fa5165b67446be1f52f1a4c6ca023f2d33de3e59
progspectar <progspectar@gmail.com>
2024-07-21 00:10:04 +0700

    Очереди и стеки [#505157] исправление

--- src/main/java/ru/job4j/algo/Brackets.java
+++ src/main/java/ru/job4j/algo/Brackets.java
@@ -1,22 +1,20 @@
 package ru.job4j.algo;
 
 import java.util.Deque;
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
 
 class Brackets {
     public boolean isValid(String s) {
-        Map<Character, Character> map = new HashMap<>();
-        map.put('(', ')');
-        map.put('{', '}');
-        map.put('[', ']');
         Deque<Character> stack = new LinkedList<>();
         for (char c : s.toCharArray()) {
             if (!stack.isEmpty() && stack.peek() == c) {
                 stack.pop();
-            } else if (map.get(c) != null) {
-                stack.push(map.get(c));
+            } else if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
             } else {
                 return false;
             }
