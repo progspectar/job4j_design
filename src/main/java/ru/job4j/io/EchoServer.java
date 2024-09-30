9ba20205813cf6de7e6233b3be375a61681ceecd
progspectar <progspectar@gmail.com>
2024-09-30 21:57:24 +0700

    4. Slf4j - вывод exception.

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,8 +1,5 @@
 package ru.job4j.io;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,9 +8,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -27,18 +22,14 @@ public class EchoServer {
                     }
                     if (string.contains("/?msg=Exit")) {
                         server.close();
-                    } else if (string.contains("/?msg=err")) {
-                        throw new IOException();
                     } else if (string.contains("/?msg=Hello")) {
                         output.write("Hello\r\n\r\n".getBytes());
                     } else {
-                        output.write("What\r\n\r\n".getBytes());
+                       output.write("What\r\n\r\n".getBytes());
                     }
                     output.flush();
                 }
             }
-        } catch (IOException e) {
-            LOG.error("Error", e);
         }
     }
 }
