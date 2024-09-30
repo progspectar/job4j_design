ce314a68d0fed815c0ceb2ab5630318c289b4d0f
progspectar <progspectar@gmail.com>
2024-09-30 21:11:52 +0700

    3. Slf4j - вывод переменных.

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,15 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte age = 1;
+        short velocity = 2;
+        int counter = 3;
+        long lcounter = 4L;
+        float fcounter = 5F;
+        double dcounter = 6;
+        char ch = '7';
+        boolean bool = true;
+        LOG.debug("Age: {}, velocity: {}, counter: {}, lcounter: {}, fcounter: {}, dcounter: {}, ch: {}, bool: {}",
+                age, velocity, counter, lcounter, fcounter, dcounter, ch, bool);
     }
 }
